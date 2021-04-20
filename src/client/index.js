2ea544d9c109d8a928452fcb1ccb9b48d1a9a9f3
waseem azmy <waseemazmy21@gmail.com>
2021-04-20 13:01:07 +0200

    add style and css loaders

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/resets.css'
+import './styles/base.css'
+import './styles/footer.css'
+import './styles/form.css'
+import './styles/header.css'
+
+
 console.log(checkForName);
 
 alert("I EXIST")
